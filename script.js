84def967f9f3df78dd84bde299a7cfd3b6cb0a94
abadin-Fahim <joynalabadinfahim@gmail.com>
2023-03-08 12:57:41 +0100

    html and css code for calclulator

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+console.log("connected")
+
+const display = document.getElementById('display');
+
+function addToDisplay(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
